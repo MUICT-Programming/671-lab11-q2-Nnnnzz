6adc5be514e42a70fa9145b6298df0f0777ab71a
Nine <132139758+Nnnnzz@users.noreply.github.com>
2024-10-19 23:22:36 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
 # YOUR CODE HERE
+n = int(input())
+lst1 = []
+lst2 = []
+
+for i in range(n) :
+  x = int(input())
+  lst1.append(x)
+for i in range(n) :
+  y = int(input())
+  lst2.append(y)
+  
+def summation(a,b) :
+  update_list = []
+  for i in range(n) :
+    update_list.append(a[i]+b[i])
+  return update_list
+  
+print(summation(lst1,lst2))
+
+def find_min_max(update_list) :
+  min_value = min(update_list)
+  max_value = max(update_list)
+  min_max = (f'({min_value}, {max_value})')
+  return min_max
+
+print(find_min_max(summation(lst1,lst2)))
